d3da02258642e3fcb8968c27933e727d9974048b
zonda-tsai <dallas.tsai9@gmail.com>
2025-05-19 00:34:47 +0800

    Create initial.sh

---
+++ initial.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+gcc -O3 "$DIR/settings.c" -o "$DIR/settings" >/dev/null 2>&1
+gcc -O3 "$DIR/compile.c" -o "$DIR/compile" >/dev/null 2>&1
+gcc -O3 "$DIR/run.c" -o "$DIR/run" >/dev/null 2>&1
+gcc -O3 "$DIR/clean.c" -o "$DIR/clean" >/dev/null 2>&1
+"$DIR/settings" -initial
+mkdir -p "$HOME/bin"
+mv "$DIR"/{settings,compile,run,clean} "$HOME/bin"
+mkdir -p "$HOME/.zonda.ide"
+mv "$DIR"/{settings.c,compile.c,run.c,clean.c} "$HOME/.zonda.ide"
+
+if ! grep -q 'export PATH="$HOME/bin:$PATH"' "$HOME/.bashrc"; then
+    echo 'export PATH="$HOME/bin:$PATH"' >> "$HOME/.bashrc"
+fi
+source $HOME/.bashrc
+
+printf "\e[33;1m[FINISHED]\e[0m\n"
