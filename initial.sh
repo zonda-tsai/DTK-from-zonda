f029cc0b49ccc8a61bad03566b173795ece5bff7
zonda-tsai <dallas.tsai9@gmail.com>
2025-05-22 16:58:48 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -11,9 +11,9 @@ gcc -O3 "$DIR/run.c" -o "$DIR/run" >/dev/null 2>&1
 gcc -O3 "$DIR/char_ins.c" -o "$DIR/char_ins" >/dev/null 2>&1
 "$DIR/settings" -initial
 mkdir -p "$HOME/bin"
-mv "$DIR"/{settings,compile,run, char_ins} "$HOME/bin"
+mv "$DIR/settings" "$DIR/compile" "$DIR/run" "$DIR/char_ins" "$HOME/bin"
 mkdir -p "$HOME/.zonda.ide"
-mv "$DIR"/{settings.c,compile.c,run.c, char_ins.c} "$HOME/.zonda.ide"
+mv "$DIR/settings.c" "$DIR/compile.c" "$DIR/run.c" "$DIR/char_ins.c" "$HOME/.zonda.ide"
 
 if ! grep -q 'export PATH="$HOME/bin:$PATH"' "$HOME/.bashrc"; then
     echo 'export PATH="$HOME/bin:$PATH"' >> "$HOME/.bashrc"
