d47225c59ee3d051ae3f9db527fbf9516342b36b
zonda-tsai <dallas.tsai9@gmail.com>
2025-05-23 01:52:03 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -9,11 +9,12 @@ gcc -O3 "$DIR/settings.c" -o "$DIR/settings" >/dev/null 2>&1
 gcc -O3 "$DIR/compile.c" -o "$DIR/compile" >/dev/null 2>&1
 gcc -O3 "$DIR/run.c" -o "$DIR/run" >/dev/null 2>&1
 gcc -O3 "$DIR/char_ins.c" -o "$DIR/char_ins" >/dev/null 2>&1
+gcc -O3 "$DIR/tfmanager.c" -o "$DIR/tfmanager" >/dev/null 2>&1
 "$DIR/settings" -initial
 mkdir -p "$HOME/bin"
-mv "$DIR/settings" "$DIR/compile" "$DIR/run" "$DIR/char_ins" "$HOME/bin"
+mv "$DIR/settings" "$DIR/compile" "$DIR/run" "$DIR/char_ins" "$DIR/tfmanager" "$HOME/bin"
 mkdir -p "$HOME/.zonda.ide"
-mv "$DIR/settings.c" "$DIR/compile.c" "$DIR/run.c" "$DIR/char_ins.c" "$DIR/README.md" "$HOME/.zonda.ide"
+mv "$DIR/settings.c" "$DIR/compile.c" "$DIR/run.c" "$DIR/char_ins.c" "$DIR/tfmanager.c" "$DIR/README.md" "$HOME/.zonda.ide"
 
 if ! grep -q 'export PATH="$HOME/bin:$PATH"' "$HOME/.bashrc"; then
     echo 'export PATH="$HOME/bin:$PATH"' >> "$HOME/.bashrc"
@@ -22,4 +23,4 @@ source $HOME/.bashrc
 export PATH="$HOME/bin:$PATH"
 
 rm -rf "$DIR"
-printf "\e[33;1m[FINISHED]\e[0m\n"
+printf "\n\e[33;1m[FINISHED]\e[0m\n"
