9c6c2c017e3a6d367e6a5ede259eed4107a964d0
zonda-tsai <dallas.tsai9@gmail.com>
2025-06-01 14:15:59 +0800

    Add files via upload

--- char_ins.c
+++ char_ins.c
@@ -7,13 +7,38 @@ void help(){
 	printf("For document, please use char_ins < \'file_name\'\n");
 	printf("New-line, tab, return and end of string will be represent by \e[34m\\n\e[0m, \e[34m\\t\e[0m and \e[34m\\r\e[0m \e[34m\\0\e[0m\n");
 	printf("Spaces will be represent by \'\e[100m \e[0m\'\n");
-	printf("Control ASCII will be shown as \e[45m(Ctrl ASCII: )\e[0m\n");
-	printf("For all the others, excluding digits, alphabets and punctuations, will be represent by \e[41m(ASCII: )\e[0m\n\n");
+	printf("Control ASCII will be shown as \e[45m\\x<HEX>\e[0m\n");
+	printf("For all the others, excluding digits, alphabets and punctuations, will be represent by \e[41m(\\x<HEX>)\e[0m\n\n");
+	printf("To show ascii-table by char_ins --ascii_table\n");
+}
+
+void ascii(){
+	int i, j, cntrl = 0;
+	unsigned char ctrl[33][5] = {"\\0", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "\\a", "\\b", "\\t", "\\n", "\\v", "\\f", "\\r", "SO", "SI", "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM", "SUB", "ESC", "FS", "GS", "RS", "US", "DEL"};
+	printf("  \e[33m");
+	for(i = 0 ; i < 16 ; i++)
+		printf("%-5x", i);
+	printf("\e[0m\n");
+	for(i = 0 ; i < 8 ; i++){
+		printf("\e[33m%-2x\e[0m", i);
+		for(j = 0 ; j < 16 ; j++){
+			if(iscntrl((char)(i * 16 + j)))
+				printf("%-5s", ctrl[cntrl++]);
+			else
+				printf("%-5c", (char)(16 * i + j));
+		}
+		printf("\n");
+	}
 }
 
 int main(int argc, char* argv[]){
+	
 	if(argc == 2 && strcmp(argv[1], "--help") == 0){
 		help();
+		return 0;
+	}
+	else if(argc == 2 && strcmp(argv[1], "--ascii_table") == 0){
+		ascii();
 		return 0;
 	}
 	else if(argc != 1){
@@ -22,8 +47,8 @@ int main(int argc, char* argv[]){
 	}
 	int i;
 	unsigned long long n = 0, news = 0, tabs = 0, ctrls = 0, spaces = 0, unknows = 0, normals = 0, returns = 0;
-	char temp[1000001] = {0};
-	while(fgets(temp, 1000000, stdin) != NULL){
+	unsigned char temp[1000001] = {0};
+	while(fgets((char*)temp, 1000000, stdin) != NULL){
 		for(i = 0 ; temp[i] != 0 && i < 1000000 ; i++){
 			if(iscntrl(temp[i])){
 				ctrls++;
@@ -35,12 +60,18 @@ int main(int argc, char* argv[]){
 					tabs++;
 					printf("\e[34m\\t\e[0m");
 				}
-				else if(temp[i] == 13){
+				else if(temp[i] == '\r'){
 					returns++;
 					printf("\e[34m\\r\e[0m");
 				}
+				else if(temp[i] == '\a')
+					printf("\e[34m\\a\e[0m");
+				else if(temp[i] == '\b')
+					printf("\e[34m\\b\e[0m");
+				else if(temp[i] == '\v')
+					printf("\e[34m\\v\e[0m");
 				else
-					printf("\e[45m(Ctrl ASCII: %d)\e[0m", temp[i]);
+					printf("\e[45m\\x%x\e[0m", temp[i]);
 			}
 			else if(isspace(temp[i])){
 				spaces++;
@@ -48,7 +79,7 @@ int main(int argc, char* argv[]){
 			}
 			else if(!isalpha(temp[i]) && !isdigit(temp[i]) && !ispunct(temp[i])){
 				unknows++;
-				printf("\e[41m%c(ASCII: %d)\e[0m", temp[i], temp[i]);
+				printf("\e[41m\\x%x\e[0m", temp[i]);
 			}
 			else{
 				normals++;
