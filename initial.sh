68fa5464150c02ba2d5a42c416412ea06b765ba0
zonda-tsai <dallas.tsai9@gmail.com>
2025-06-10 19:54:08 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -14,13 +14,14 @@ gcc -O3 "$DIR/clean.c" -o "$DIR/clean" >/dev/null 2>&1
 gcc -O3 "$DIR/char_ins.c" -o "$DIR/char_ins" >/dev/null 2>&1
 gcc -O3 "$DIR/tfmanager.c" -o "$DIR/tfmanager" >/dev/null 2>&1
 gcc -O3 "$DIR/+.c" -o "$DIR/+" >/dev/null 2>&1
+gcc -O3 "$DIR/tutorial.c" -o "$DIR/tutorial" >/dev/null 2>&1
 "$DIR/settings" -initial
 mkdir -p "$HOME/bin"
 mv "$DIR/settings" "$DIR/compile" "$DIR/run" "$DIR/clean" "$DIR/char_ins" "$DIR/tfmanager" "$DIR/+" "$HOME/bin"
-mkdir -p "$HOME/.zonda.ide"
-mv "$DIR/README.md" "$HOME/.zonda.ide"
-mkdir -p "$HOME/.zonda.ide/sources"
-mv "$DIR/settings.c" "$DIR/compile.c" "$DIR/run.c" "$DIR/clean.c" "$DIR/char_ins.c" "$DIR/tfmanager.c" "$DIR/+.c" "$HOME/.zonda.ide/sources"
+mkdir -p "$HOME/.zonda.due"
+mv "$DIR/tutorial" "$HOME/.zonda.due"
+mkdir -p "$HOME/.zonda.due/sources"
+mv "$DIR/settings.c" "$DIR/tutorial.c" "$DIR/compile.c" "$DIR/run.c" "$DIR/clean.c" "$DIR/char_ins.c" "$DIR/tfmanager.c" "$DIR/+.c" "$HOME/.zonda.due/sources"
 
 if ! grep -q 'export PATH="$HOME/bin:$PATH"' "$HOME/.bashrc"; then
     echo 'export PATH="$HOME/bin:$PATH"' >> "$HOME/.bashrc"
@@ -31,3 +32,4 @@ export PATH="$HOME/bin:$PATH"
 rm -rf "$DIR"
 printf "\n\e[33;1m[FINISHED]\e[0m\n"
 printf "If you can't successfully use compile or othere executable files, please type 'export PATH=\"$HOME/bin:$PATH\"' in your terminal\n"
+printf "If you are new to this, please type 'settings --help' for tutorial\n"
